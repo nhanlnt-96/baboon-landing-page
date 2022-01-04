63a8a46c5bb212fecb4f230a93d609cf1d6fbaba
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-05 01:06:21 +0700

    [about]: Complete change layout of about section

--- src/components/about/AboutComp.js
+++ src/components/about/AboutComp.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import {aboutData} from "../../configs/aboutData";
+import AboutImg1 from '../../assets/imgs/aboutImage1.png';
+import AboutImg2 from '../../assets/imgs/aboutImage2.png';
+import AboutImg3 from '../../assets/imgs/aboutImage3.png';
+import AboutImg4 from '../../assets/imgs/aboutImage4.png';
 
 import './AboutComp.scss';
+
+const aboutImg = [AboutImg1, AboutImg2, AboutImg3, AboutImg4];
 
 const AboutComp = () => {
   return (
@@ -16,24 +22,24 @@ const AboutComp = () => {
           </div>
         </Row>
         <Row className="about-comp-content">
-          {
-            aboutData.map((val, key) => (
-              <Col xl={6} ld={6} md={6} sm={12} key={key} className="item d-flex flex-column align-items-center">
-                <div className="item-img">
-                  <div className="img-border d-flex justify-content-center align-items-center">
-                    <img src={val.img} alt="baboon-about-img"/>
-                  </div>
-                </div>
-                <div className="item-content">
-                  {
-                    val.content.map((item, index) => (
-                      <p key={index} className="content">{item}</p>
-                    ))
-                  }
-                </div>
-              </Col>
-            ))
-          }
+          <Col xl={6} lg={7} md={7} sm={12} className="left-side d-flex justify-content-center align-items-center">
+            <div className="left-side-container">
+              {
+                aboutData.map((val, index) => (
+                  <p key={index} className="content">{val}</p>
+                ))
+              }
+            </div>
+          </Col>
+          <Col xl={6} lg={5} md={5} sm={12} className="right-side d-flex justify-content-center align-items-center">
+            <div className="right-side-container">
+              {
+                aboutImg.map((val, index) => (
+                  <img key={index} src={val} alt="about-baboon"/>
+                ))
+              }
+            </div>
+          </Col>
         </Row>
       </Container>
       <Row className="leaf-bottom"/>
