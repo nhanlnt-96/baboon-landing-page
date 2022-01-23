9cafd712cddd0b01dfe90eb74fd74ba9998b2be5
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-23 21:40:43 +0700

    [master]: Update site text and background

--- src/configs/roadmapData.js
+++ src/configs/roadmapData.js
@@ -6,28 +6,28 @@ export const roadmapData = [
   },
   {
     percent: "20%",
-    question: ["What kind of monkey flies to school?...", " A hot air baboon."],
-    answer: ["We are gaining momentum!!!", "10 random owners will each receive a 1:1 special BABBLING BABOONS Club NFT."]
+    question: ["What kind of monkey flies to school?...", " A hot air Baboon."],
+    answer: ["We are gaining momentum!!!", "10 random owners will each receive a 1:1 special Babbling Baboon Club NFT."]
   },
   {
     percent: "40%",
-    question: ["Why don't the baboons in the jungle play poker anymore?...", " There are just too many cheetahs"],
+    question: ["Why don't the Baboon in the jungle play poker anymore?...", " There are just too many cheetahs"],
     answer: ["The community wallet is open!", "The community wallet will be used for a variety of things for ensuring persistent survival."]
   },
   {
     percent: "50%",
-    question: ["Wanna know why those baboons are always drinking in the park?...", " It’s the only place with monkey" +
+    question: ["Wanna know why those Baboon are always drinking in the park?...", " It’s the only place with monkey" +
     " bars!"],
-    answer: ["A Cool Cats NFT will be airdropped to a lucky BABBLING BABOONS owner with a estimated value of 8 Eth = $35,000 USD"]
+    answer: ["A Cool Cats NFT will be airdropped to a lucky Babbling Baboon owner with a estimated value of 8 Eth = $35,000 USD"]
   },
   {
     percent: "80%",
-    question: ["Why do Baboons have big nostrils?...", " They have big fingers."],
-    answer: ["The BABBLING BABOONS Club merch store will open. We will be giving away 100 items to holders who will have their choice in which item they would like."]
+    question: ["Why do Baboon have big nostrils?...", " They have big fingers."],
+    answer: ["The Babbling Baboon Club merch store will open. We will be giving away 100 items to holders who will have their choice in which item they would like."]
   },
   {
     percent: "100%",
     question: ["What do you call an exploding monkey?...", " a baBOOM!"],
-    answer: ["SOLD OUT!", "An exclusive BABBLING BABOONS play-to-earn game will be released with community prizes."]
+    answer: ["SOLD OUT!", "An exclusive Babbling Baboon play-to-earn game will be released with community prizes."]
   },
 ]
